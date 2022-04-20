a3e238fa9dc98cab185c7dc43be55d3a8f9cc7f4
Niklas Deworetzki <niklas.deworetzki@mni.thm.de>
2022-04-20 10:53:09 +0200

    Continue development of debugger.

--- source/src/debug/debugger.cpp
+++ source/src/debug/debugger.cpp
@@ -1,17 +1,8 @@
 
-#include <set>
+#include <iostream>
 #include "debugger.h"
 
 namespace Machine {
-
-    struct debugger_state {
-        std::set<bool> breakpoints;
-        uint32_t remaining_steps;
-        bool continue_running;
-    };
-
-    typedef bool (*debugger_hook)(VM &, debugger_state &);
-
 
     static bool requires_user_interaction(const VM &vm, const debugger_state &state);
 
@@ -19,15 +10,105 @@ namespace Machine {
 
     static void step_debugger_state(debugger_state &state);
 
+
     void run_with_debugger(VM &vm) {
-        debugger_state state{};
+        debugger_state state;
         do {
             if (requires_user_interaction(vm, state)) {
                 interact_with_user(vm, state);
+                if (state.exit) {
+                    break;
+                }
             }
             vm.step();
             step_debugger_state(state);
         } while (vm.running);
     }
 
+
+    static bool requires_user_interaction(const VM &vm, const debugger_state &state) {
+        if (state.continue_running) return state.breakpoints.contains(vm.pc);
+        else return state.remaining_steps == 0;
+    }
+
+    static void step_debugger_state(debugger_state &state) {
+        if (state.remaining_steps > 0) {
+            state.remaining_steps--;
+        }
+    }
+
+
+    static void split(const std::string &str, std::vector<std::string> &result_buffer) {
+        std::istringstream stream(str);
+
+        std::string token;
+        while (std::getline(stream, token, ' ')) {
+            if (!token.empty()) {
+                result_buffer.push_back(token);
+            }
+        }
+    }
+
+    static void print_machine_state(VM &vm) {
+        // dir = %s, pc = %d, br = %d, sp = %d, fp = %d
+        std::cout << "dir = " << (vm.dir == Direction::Forward ? "Forward" : "Backward")
+                  << " pc = " << vm.pc
+                  << " br = " << vm.br
+                  << " sp = " << vm.sp
+                  << " fp = " << vm.fp
+                  << std::endl;
+
+        bool elements_hidden;
+        std::cout << "Stack: ";
+        if (vm.fp > 0) {
+            for (int32_t addr = vm.sp - 1; addr >= vm.fp; addr--) {
+                std::cout << vm.stack[addr] << " ";
+            }
+            elements_hidden = true;
+        } else {
+            int32_t lower_bound = std::max(0, vm.sp - 10);
+            for (int32_t addr = vm.sp - 1; addr >= lower_bound; addr--) {
+                std::cout << vm.stack[addr] << " ";
+            }
+            elements_hidden = lower_bound != 0;
+        }
+
+        if (elements_hidden) {
+            std::cout << "...";
+        }
+
+        std::cout << std::endl;
+    }
+
+    static void interact_with_user(VM &vm, debugger_state &state) {
+        std::string input;
+        std::vector<std::string> command;
+
+        print_machine_state(vm);
+
+        continue_t may_continue = PROMPT_USER;
+        do {
+            std::cout << "> " << std::flush;
+            std::getline(std::cin, input);
+
+            if (std::cin.eof()) {
+                std::cout << "Input closed. Exiting.";
+                state.exit = true;
+            } else {
+                if (input.empty()) { // Try to re-perform last action.
+                    if (state.last_input.empty()) {
+                        continue; // No previous input cached. Prompt user again.
+                    } else {
+                        input = state.last_input;
+                    }
+                }
+
+                command.clear();
+                split(input, command);
+                if (available_commands.contains(input)) {
+                    may_continue = available_commands[input](vm, state, command);
+                }
+            }
+        } while (may_continue == PROMPT_USER && !state.exit);
+    }
 }
